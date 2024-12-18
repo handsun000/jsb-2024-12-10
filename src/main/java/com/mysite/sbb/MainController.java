fa93e2ef99ec35cc9a50b0688cd7854a4aa9800c
son <handsun000@gmail.com>
2024-12-18 12:24:08 +0900

    redirect:/question/list

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,17 +2,11 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void home() {
-        System.out.println("home");
-    }
-    @GetMapping("/about")
-    @ResponseBody
-    public String about() {
-        return "about";
+    public String root() {
+        return "redirect:/question/list";
     }
 }
