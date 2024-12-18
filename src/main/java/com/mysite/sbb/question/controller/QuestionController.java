6a187b7461ed4c89cfde84c84650a31e8cf02fc2
son <handsun000@gmail.com>
2024-12-18 14:18:09 +0900

    question detail

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,5 +26,13 @@ public class QuestionController {
 
         model.addAttribute("questions", questions);
         return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable long id, Model model) {
+        Question question = questionService.findById(id);
+
+        model.addAttribute("question", question);
+        return "detail";
     }
 }
--- src/main/java/com/mysite/sbb/question/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -3,9 +3,12 @@ package com.mysite.sbb.question.service;
 import com.mysite.sbb.question.entity.Question;
 import com.mysite.sbb.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,12 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question findById(long id) {
+        Optional<Question> op = questionRepository.findById(id);
+
+        if (op.isPresent()) return op.get();
+        else throw new RuntimeException("데이터가 없습니다.");
     }
 }
