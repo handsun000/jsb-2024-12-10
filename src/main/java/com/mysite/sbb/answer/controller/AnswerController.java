be9b30619f7f322b8ff4e4acd7f92e02260fb424
son <handsun000@gmail.com>
2024-12-19 12:30:25 +0900

    @Valid

--- src/main/java/com/mysite/sbb/answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/controller/AnswerController.java
@@ -1,12 +1,16 @@
 package com.mysite.sbb.answer.controller;
 
 import com.mysite.sbb.answer.entity.Answer;
+import com.mysite.sbb.answer.entity.form.AnswerForm;
 import com.mysite.sbb.answer.repository.AnswerRepository;
 import com.mysite.sbb.answer.service.AnswerService;
 import com.mysite.sbb.question.entity.Question;
 import com.mysite.sbb.question.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,9 +27,15 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(String content, @PathVariable long id) {
+    public String createAnswer(Model model, @PathVariable long id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = questionService.findById(id);
-        answerService.write(question, content);
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question/question_detail";
+        }
+
+        answerService.write(question, answerForm.getContent());
         return "redirect:/question/detail/%s".formatted(id);
     }
 }
