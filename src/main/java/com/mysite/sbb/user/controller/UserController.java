9839e21135917e7de3c5ca4e198365ccd698782e
son <handsun000@gmail.com>
2024-12-19 11:35:42 +0900

    singup

---
+++ src/main/java/com/mysite/sbb/user/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.mysite.sbb.user.controller;
+
+import com.mysite.sbb.user.form.UserCreateForm;
+import com.mysite.sbb.user.service.SiteUserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final SiteUserService siteUserService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        siteUserService.create(userCreateForm.getUsername(), userCreateForm.getPassword1(), userCreateForm.getEmail());
+
+        return "redirect:/";
+    }
+}
